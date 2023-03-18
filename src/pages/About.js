489240180907c0886c6662414f5d150dacde2597
BenjaminLin808 <benjaminlin808@gmail.com>
2023-03-17 17:52:41 -0700

    react practice project #15

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis animi
+        exercitationem, nihil neque reprehenderit rem saepe in optio velit esse.
+        Ex aut error, saepe totam nulla culpa iusto sapiente consequatur?
+      </p>
+    </section>
+  );
+};
+
+export default About;
